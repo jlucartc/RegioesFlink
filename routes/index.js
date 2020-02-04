5829e8fe1f89e330e20ff58f828bb62999b52672
jlucartc <jlucartc@gmail.com>
2020-02-04 16:43:07 -0300

    migrando para outro projeto feito com react

--- routes/index.js
+++ routes/index.js
@@ -1,37 +1,32 @@
+require('dotenv').config()
 var express = require('express');
 const { Sequelize, Datatypes } = require('sequelize');
 
-const key = require('knex')({
-  client: 'pg',
+const knex = require('knex')({
+  client: process.env.DB_CLIENT,
   connection: {
-    host: 'localhost',
-    user: 'luca',
-    password: 'root',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
     database: 'mydb'
   }
 });
-
-const sequelize = new Sequelize('mydb','luca','root',{
-  host: 'localhost',
-  port : '5432',
-  database : 'mydb',
-  dialect: 'postgres'
-})
 
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
 
-    var pointsAndRegions = new Array()
+    res.render('default')
+    /*var pointsAndRegions = new Array()
 
-    key('regioes').select().then( data => {
+    knex('regioes').select().then( data => {
 
       data.forEach( val => {
 
         pointsAndRegions.push(val)
 
       })
-      return key('raios').select()
+      return knex('raios').select()
 
     }).then(
 
@@ -43,26 +38,14 @@ router.get('/', function(req, res, next) {
 
         })
 
+        console.log(pointsAndRegions)
         res.render('index', { title: 'Express' , data : pointsAndRegions })
 
       }
 
-    )
-
-    /*key('raios').select()
-    .then( data => {
-
-      storedPoints = data
-      console.log(storedPoints)
-
-    }, err => { console.log(err)})
-
-    //var pointsAndRegions = storedPoints.concat(storedPolygons)
-
-    res.render('index', { title: 'Express' , data : pointsAndRegions });*/
+    )*/
 
 });
-
 router.post('/saveData',function(req,res,next){
 
     if(req.body.type == "regiao"){
@@ -73,7 +56,7 @@ router.post('/saveData',function(req,res,next){
 
       if(req.body.data.length >= 3){
 
-        var insertPolygon = key('regioes').insert(key.raw(` (nome,regiao) values ('${nome}', polygon(path '${polygon}'))`))
+        var insertPolygon = knex('regioes').insert(knex.raw(` (nome,regiao) values ('${nome}', polygon(path '${polygon}'))`))
         .then(
           res => {
 
@@ -102,7 +85,7 @@ router.post('/saveData',function(req,res,next){
 
       var raio = req.body.raio
 
-      var insertPoints = key('raios').insert(key.raw(` (nome,raio) values ('${nome}', circle('${ponto}',${raio}))`))
+      var insertPoints = knex('raios').insert(knex.raw(` (nome,raio) values ('${nome}', circle('${ponto}',${raio}))`))
       .then(res => { console.log(`Raio '${nome}'cadastrado com sucesso!`)}, err => { console.log('Erro no cadastro do raio')})
 
       //console.log(insertPoints)
@@ -110,6 +93,97 @@ router.post('/saveData',function(req,res,next){
     }
 
     res.sendStatus(200)
+
+});
+router.get('/getRegioes',function(req,res,next){
+
+    var regioes = new Array()
+
+    knex('regioes').select().then(
+
+      data => {
+
+        data.forEach(val => {
+
+          regioes.push(val)
+
+        })
+
+        res.json(regioes)
+
+      }
+
+    )
+
+});
+router.get('/getRaios',function(req,res,next){
+
+  var raios = new Array()
+
+  knex('raios').select().then(
+
+    data => {
+
+      data.forEach(val => {
+
+        raios.push(val)
+
+      })
+
+      res.json(raios)
+
+    }
+
+  )
+
+});
+router.post('/saveRegiao',function(req,res,next){
+
+  var nome  = req.body.nome
+
+  var regiao = JSON.stringify(req.body.regiao).replace(/\[/g,'(').replace(/\]/g,')')
+  regiao = regiao.slice(1,regiao.length-1)
+
+  var query = knex('regioes').insert(knex.raw(` values ('${nome}',polygon(path '${regiao}')) `))
+  .then(
+    success => {
+      console.log(`Região ${nome} inserida no banco`)
+      res.json({msg : `Região ${nome} inserida no banco`})
+    },
+    err => {
+      console.log(`Erro na inserção da região ${nome}`)
+      res.json({msg : `Região ${nome} inserida no banco`})
+    }
+  )
+
+  console.log(query)
+
+});
+router.post('/saveRaio',function(req,res,next){
+
+  var nome = req.body.nome
+  var raio = req.body.raio
+  var ponto = JSON.stringify(req.body.ponto).replace(/\]/g,')').replace(/\[/g,'(')
+  ponto = ponto.slice(1,ponto.length-1)
+
+  var query = knex('raios').insert(knex.raw(` values ('${nome}', circle('${ponto}',${raio})) `)).then(
+
+    success => {
+
+      console.log(`Raio ${nome} salvo com sucesso!`)
+
+      res.json({msg : `Raio ${nome} salvo com sucesso!`})
+
+    },
+    err => {
+
+      console.log(`Erro no cadastro de raio ${nome}`)
+
+      res.json({msg : `Erro no cadastro de raio ${nome}`})
+
+    }
+
+  )
 
 });
 
